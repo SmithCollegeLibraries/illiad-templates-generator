2c11f00f9fff089781d57441baaacb5a02c935c8
Tristan Chambers <tchambers@smith.edu>
2017-08-16 10:50:11 -0400

    Add input and output folders and logging

--- make-illiad-templates.py
+++ make-illiad-templates.py
@@ -1,9 +1,12 @@
 # Load the jinja library's namespace into the current module.
 import jinja2
 import glob, os
-# os.chdir(".")
+import logging
+logging.basicConfig(level=logging.INFO)
 
-    
+inputPath = 'jinjafied-illiad-templates'
+outputPath = 'rendered-illiad-templates'
+
 # In this case, we will load templates off the filesystem.
 # This means we must construct a FileSystemLoader object.
 # 
@@ -16,17 +19,20 @@ templateLoader = jinja2.FileSystemLoader( searchpath="." )
 #   parse our templates.  We pass in the loader object here.
 templateEnv = jinja2.Environment( loader=templateLoader )
 
-for file in glob.glob("*.html"):
+for inputFile in glob.glob(inputPath + '/' + "*.html"):
+    logging.info('Parsing %s' % inputFile)
+
     # Read the template file using the environment object.
     # This also constructs our Template object.
-    template = templateEnv.get_template( file )
+    template = templateEnv.get_template( inputFile )
 
     # Specify any input variables to the template as a dictionary.
     templateVars = { "title" : "Test Example",
                      "description" : "A simple inquiry of function." }
-    with open(fname, 'w') as rendered-illiad-templates + '/' + file:
+    outputFilePath = outputPath + '/' + os.path.basename( inputFile )
+
+    with open(outputFilePath, 'w') as f:
         # Finally, process the template to produce our final text.
         outputText = template.render( templateVars )
+        logging.info('Writing %s' % outputFilePath)
         f.write(outputText)
-
-        print(outputText)
